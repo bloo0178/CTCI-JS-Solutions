bb562203a6ee19f633693b41c28354b8e9c61200
bloo0178 <bloo0178@gmail.com>
2019-03-14 11:45:02 -0400

    Updated tree node tests

--- 4-Trees-and-Graphs/treeNode.test.js
+++ 4-Trees-and-Graphs/treeNode.test.js
@@ -1,9 +1,17 @@
 const Node = require("./treeNode");
 
+let testTree = new Node(1); 
+testTree.left = new Node(2);
+testTree.left.left = new Node(3); 
+testTree.left.right = new Node(4);
+testTree.right = new Node(5);
+testTree.right.left = new Node(6);
+testTree.right.right = new Node(7);
+testTree.right.right.left = new Node(0);
+
 test("build a tree", () => {
   let tree = new Node(1);
   tree.left = new Node(2);
-  //tree.right = new Node(3);
   expect(tree)
     .toEqual({
       left: { left: null, right: null, val: 2 },
@@ -11,3 +19,8 @@ test("build a tree", () => {
       val: 1
     });
 });
+
+test("nested val", () => {
+    expect(testTree.right.right.left.val)
+    .toBe(0);
+})
