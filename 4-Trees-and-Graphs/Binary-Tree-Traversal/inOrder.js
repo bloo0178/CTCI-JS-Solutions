5dd808c2b2dd6074ac15f82d1a5e7e97f1d1615a
bloo0178 <bloo0178@gmail.com>
2019-03-14 12:05:21 -0400

    Added in-order binary tree traversal and tests.

---
+++ 4-Trees-and-Graphs/Binary-Tree-Traversal/inOrder.js
@@ -0,0 +1,27 @@
+const Node = require("../treeNode");
+
+/*function inOrder(node) {
+    if(node !== null) {
+        inOrder(node.left);
+        return node.val;
+        inOrder(node.right); 
+    }
+}*/
+
+function printInOrder(root) {
+  result = [];
+
+  function inOrder(node) {
+    if (node !== null) {
+      inOrder(node.left);
+      result.push(node.val);
+      inOrder(node.right);
+    }
+  }
+
+  inOrder(root);
+
+  return result;
+}
+
+module.exports = printInOrder;
---
+++ 4-Trees-and-Graphs/Binary-Tree-Traversal/inOrder.test.js
@@ -0,0 +1,16 @@
+const Node = require('../treeNode');
+const printInOrder = require('./inOrder');
+
+let testTree = new Node(1); 
+testTree.left = new Node(2);
+testTree.left.left = new Node(3); 
+testTree.left.right = new Node(4);
+testTree.right = new Node(5);
+testTree.right.left = new Node(6);
+testTree.right.right = new Node(7);
+testTree.right.right.left = new Node(0);
+
+test('print test tree', () => {
+    expect(printInOrder(testTree))
+    .toEqual([3,2,4,1,6,5,0,7])
+})
