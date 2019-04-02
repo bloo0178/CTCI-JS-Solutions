ff13d30620f4eed575ae99492a86c72769191f7f
bloo0178 <bloo0178@gmail.com>
2019-04-02 10:57:15 -0400

    Added random pivot to quicksort algorithm.

--- grokking-algorithms/ch4-quicksort/quicksort.js
+++ grokking-algorithms/ch4-quicksort/quicksort.js
@@ -1,20 +1,35 @@
+/*
+
+Uses a random pivot which results in best case runtime being the average case runtime. This quicksort takes O(n*log(n)) time to complete. 
+
+*/
+
 function quicksort(arr) {
 
     // base case
     if (arr.length < 2) return arr;
     // recursive case
     else {
-        let pivot = arr[0];
+        //let pivot = arr[0];
+        let pivotIndex = getRandomVal(0, arr.length - 1);
+        let pivot = arr[pivotIndex]
         let less = [];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] <= pivot) less.push(arr[i]);
+        for (let i = 0; i < arr.length; i++) {
+            if (i !== pivotIndex && arr[i] <= pivot) less.push(arr[i]);
         }
         let greater = [];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] > pivot) greater.push(arr[i]);
+        for (let i = 0; i < arr.length; i++) {
+            if (i !== pivotIndex && arr[i] > pivot) greater.push(arr[i]);
         }
         return quicksort(less).concat([pivot].concat(quicksort(greater)));
     }
+}
+
+// Gets a random val between min and max (inclusive of both)
+function getRandomVal(min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = quicksort; 
--- grokking-algorithms/ch4-quicksort/tempCodeRunnerFile.js
+++ grokking-algorithms/ch4-quicksort/tempCodeRunnerFile.js
@@ -1,23 +1,7 @@
-function quicksort(arr) {
-
-    // base case
-    if (arr.length < 2) return arr;
-    else {
-        let pivot = arr[0];
-        let less = [];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] <= pivot) less.push(arr[i]);
-        }
-        let greater = [];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] > pivot) greater.push(arr[i]);
-        }
-        return quicksort(less).concat([pivot].concat(quicksort(greater)));
-
-    }
+function getRandomVal(min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-let arr = [4,32,6,2,7,9];
-
-console.log(quicksort(arr));
+console.log(getRandomVal(0, 1));
